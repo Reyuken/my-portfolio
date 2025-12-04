b0d619ff5853839b3d09727f1daf1d9cf166d183
Reyuken <unabiaray0@gmail.com>
2025-12-04 16:30:12 +0800

    changed active to hovered. removes box remaining active

--- src/components/cube.jsx
+++ src/components/cube.jsx
@@ -4,22 +4,18 @@ import React, { useRef, useState } from "react";
 import {useFrame } from "@react-three/fiber";
 
 export function Box(props) {
-  // This reference will give us direct access to the mesh
   const meshRef = useRef();
-  // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => (meshRef.current.rotation.y += 0.003));
-  // Return view, these are regular three.js elements expressed in JSX
   return (
     <group ref={meshRef} position={[0, 0, 0]}>
       <mesh
         {...props}
-        scale={active ? 1 : 0.5}
+        scale={hovered ? 1 : 0.5}
         // onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => (event.stopPropagation(), setHover(true), setActive(!active))}
-        onPointerOut={(event) => (event.stopPropagation(), setHover(false), setActive(!active))}
+        onPointerOver={(event) => (event.stopPropagation(), setHover(true))}
+        onPointerOut={(event) => (event.stopPropagation(), setHover(false))}
       >
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
