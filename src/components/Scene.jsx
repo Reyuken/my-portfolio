93dfd7e5dd40996112ccda5100848656876d65ae
Reyuken <unabiaray0@gmail.com>
2025-12-04 21:33:54 +0800

    testing orbitcontrol settings

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -10,13 +10,13 @@ export default function Scene() {
   return (
     <Canvas camera={{ position: [0, 3, 10], fov: 50 }}>
         <OrbitControls 
-            // minPolarAngle={Math.PI / 4}     // lower tilt limit
-            // maxPolarAngle={Math.PI / 2}     // upper tilt limit
-            // minAzimuthAngle={-Math.PI / 4}  // left limit
-            // maxAzimuthAngle={Math.PI / 4}   // right limit
-            // minDistance={3}                 // how close the camera can zoom
-            // maxDistance={10}
-            // enablePan={false}               // disable pan
+            minPolarAngle={Math.PI / 4}     // lower tilt limit
+            maxPolarAngle={Math.PI / 2}     // upper tilt limit
+            minAzimuthAngle={-Math.PI / 4}  // left limit
+            maxAzimuthAngle={Math.PI / 4}   // right limit
+            minDistance={3}                 // how close the camera can zoom
+            maxDistance={10}
+            enablePan={false}               // disable pan
         />
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 5, 2]} intensity={1} />
